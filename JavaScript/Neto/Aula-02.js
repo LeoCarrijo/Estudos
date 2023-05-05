2e49e11d84778f6659e669132ae0fe6174a36e2d
Leonardo Carrijo <lh83712@gmail.com>
2023-05-04 22:05:25 -0300

    Aula-02+
    Adição do looping While

--- JavaScript/Neto/Aula-02.js
+++ JavaScript/Neto/Aula-02.js
@@ -12,3 +12,17 @@ do{
     }while(cont <= 10)
     tab++
 }while(tab <= 10)
+
+let min = 1, max = 10
+tab = Math.floor(Math.random() * (min + max) + min)
+cont = 1
+
+while(tab <= 10){
+    console.log(`\nTabuada do ${tab}`)
+    cont = 1
+    while(cont <= 10){
+        console.log(`${tab} x ${cont} = ${tab * cont}`)
+        cont++
+    }
+    tab++
+}
