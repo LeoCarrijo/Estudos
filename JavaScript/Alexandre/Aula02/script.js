6bf77cbfbe893f8151230531d370582856267299
Leonardo Carrijo <lh83712@gmail.com>
2023-08-16 19:57:45 -0300

    Criação Aula02

---
+++ JavaScript/Alexandre/Aula02/script.js
@@ -0,0 +1,12 @@
+// VALORES
+const raio = 5.4, pi = Math.PI
+const area = pi * raio * raio
+
+// OBJETOS
+const resposta = document.querySelector('section.resposta')
+
+// PROCESSAMENTO
+
+
+// SAIDA
+resposta.innerHTML += area.toFixed(2)
