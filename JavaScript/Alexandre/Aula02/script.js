5aab60bbe74395c29230186b29341ae6b05af3d7
Leonardo Carrijo <lh83712@gmail.com>
2023-08-16 20:28:00 -0300

    Script

--- JavaScript/Alexandre/Aula02/script.js
+++ JavaScript/Alexandre/Aula02/script.js
@@ -1,12 +1,20 @@
 // VALORES
 const raio = 5.4, pi = Math.PI
-const area = pi * raio * raio
+const nome = 'Leonardo', concatenar = 'Olá'
+const valores = [7, 8, 9, 10, 11]
+
+var frase, area
 
 // OBJETOS
 const resposta = document.querySelector('section.resposta')
+const divResposta = document.createElement('div')
 
 // PROCESSAMENTO
+resposta.appendChild(divResposta)
 
+area = pi * raio * raio
+frase = `${concatenar}, ${nome}!`
 
 // SAIDA
-resposta.innerHTML += area.toFixed(2)
+divResposta.innerHTML += area.toFixed(2) + '<br>'
+divResposta.innerHTML += frase + '<br>'
