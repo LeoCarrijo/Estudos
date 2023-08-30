b8227755713719229169ea58699cdbd364073384
Leonardo Carrijo <lh83712@gmail.com>
2023-08-30 20:03:32 -0300

    Busca Binária Função

---
+++ JavaScript/Alexandre/Aula04/buscaBinaria.mjs
@@ -0,0 +1,14 @@
+function BuscarBinariamente(valorBuscado, vetor) {
+    let inicio = 0, fim = vetor.length - 1
+    while(fim >= inicio){
+        let meio = Math.floor((inicio + fim) / 2)
+        if(valorBuscado === vetor[meio]){
+            return meio
+        }else if(valorBuscado > vetor[meio]){
+            inicio = meio + 1
+        }else{
+            fim = meio - 1
+        }
+    }
+    return -1
+}
