02e2db0c25ea91f2f890d99f8210ee7faed433f8
Leonardo Carrijo <lh83712@gmail.com>
2023-11-20 10:50:36 -0300

    Update mergesort.mjs

--- JavaScript/Alexandre/Aula08/mergesort.mjs
+++ JavaScript/Alexandre/Aula08/mergesort.mjs
@@ -1,21 +1,22 @@
-import { objMotoristas } from "../Data/motoristas-obj-desord.mjs"
+function mergeSort(vetor){
 
-function mergeSort(vetor, fnComp){
     if(vetor.length < 2) return vetor
 
     let meio = Math.floor(vetor.length / 2)
+
     let vetEsq = vetor.slice(0, meio)
     let vetDir = vetor.slice(meio)
+    
+    vetEsq = mergeSort(vetEsq)
+    vetDir = mergeSort(vetDir)
 
-    vetEsq = mergeSort(vetEsq, fnComp)
-    vetDir = mergeSort(vetDir, fnComp)
-
+    
     let posEsq = 0
     let posDir = 0
     let vetRes = []
 
     while(posEsq < vetEsq.length && posDir < vetDir.length){
-        if(fnComp(vetEsq[posEsq], vetDir[posDir])){
+        if(vetEsq[posEsq] < vetDir[posDir]){
             vetRes.push(vetEsq[posEsq])
             posEsq++
         }
@@ -26,13 +27,22 @@ function mergeSort(vetor, fnComp){
     }
     let sobra
 
-    if((posEsq < posDir)){
+    if(posEsq < posDir){
         sobra = vetEsq.slice(posEsq)
     }else{
         sobra = vetDir.slice(posDir)
     }
+    
     return [ ...vetRes, ...sobra ]
 }
 
-let nomeObjOrdenado = mergeSort(objMotoristas, (elem1, elem2) => elem1.nome_motorista < elem2.nome_motorista)
-console.log(nomeObjOrdenado)
+// let nums = [ 77, 44, 22, 33, 99, 55, 88, 0, 66, 11 ]
+
+// let numsOrd = mergeSort(nums)
+
+// console.log({numsOrd})
+
+import { nomes } from "../Data/nomes-desord.mjs"
+
+let nomesOrd = mergeSort(nomes)
+console.log(nomesOrd)
